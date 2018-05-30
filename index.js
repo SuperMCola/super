0f120646ca974562b177fb7851cc5a3b1d3b20d6
SuperMCola <728501771@qq.com>
2018-05-30 21:32:50 +0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function drag(){
 	ver oDiv = document.getElementById('box');
 	     let onmousedown = function(evt){
-	     let oBtn = document.createElement('btn')	
+	     let oBtn = document.createElement('btn');
+	     let oTxt = document.createElement('txt');
 	     }
 }
